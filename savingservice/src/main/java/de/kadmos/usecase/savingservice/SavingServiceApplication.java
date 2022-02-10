e1e433762b2200ab2052edae805c3b1c5e922154
Tomas Pinto <tomas.pinto@spark.net>
2022-02-10 22:12:58 +0100

    Add saving service project

---
+++ savingservice/src/main/java/de/kadmos/usecase/savingservice/SavingServiceApplication.java
@@ -0,0 +1,13 @@
+package de.kadmos.usecase.savingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingServiceApplication.class, args);
+	}
+
+}
---
+++ savingservice/src/test/java/de/kadmos/usecase/savingservice/SavingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kadmos.usecase.savingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
