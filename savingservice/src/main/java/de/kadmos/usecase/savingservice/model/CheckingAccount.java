b4176a32d851815dac67ecf9280af8389b927013
Tomas Pinto <tomas.pinto@spark.net>
2022-02-11 17:05:45 +0100

    Add jpa annotation to entities

--- savingservice/src/main/java/de/kadmos/usecase/savingservice/model/CheckingAccount.java
+++ savingservice/src/main/java/de/kadmos/usecase/savingservice/model/CheckingAccount.java
@@ -2,23 +2,32 @@ package de.kadmos.usecase.savingservice.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
+@Entity
 public class CheckingAccount {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
   private String accountNumber;
   private BigDecimal amount;
   private LocalDateTime creationDate;
   private LocalDateTime updateDate;
+  @ManyToOne
+  @JoinColumn(name = "user_id")
   private User userId;
 }
--- savingservice/src/main/java/de/kadmos/usecase/savingservice/model/User.java
+++ savingservice/src/main/java/de/kadmos/usecase/savingservice/model/User.java
@@ -1,20 +1,31 @@
 package de.kadmos.usecase.savingservice.model;
 
+import java.awt.print.Book;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
+@Entity
 public class User {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
   private String name;
   private String surname;
   private String taxId;
+  @OneToMany(mappedBy = "user")
+  //TODO make this collection lazy, we don't need it everytime
+  private List<CheckingAccount> checkingAccounts;
 }
