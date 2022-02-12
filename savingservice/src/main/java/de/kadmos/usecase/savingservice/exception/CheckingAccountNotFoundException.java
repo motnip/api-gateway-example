39efd5c977384954d75da8544da0c07e617613ca
Tomas Pinto <tomas.pinto@spark.net>
2022-02-12 15:51:03 +0100

    Fix exceptions message

--- savingservice/src/main/java/de/kadmos/usecase/savingservice/exception/CheckingAccountNotFoundException.java
+++ savingservice/src/main/java/de/kadmos/usecase/savingservice/exception/CheckingAccountNotFoundException.java
@@ -3,7 +3,7 @@ package de.kadmos.usecase.savingservice.exception;
 public class CheckingAccountNotFoundException extends Exception {
 
   public CheckingAccountNotFoundException(String accountNumber) {
-    super(String.format("Account NUMBER {} not found",accountNumber));
+    super(String.format("Account NUMBER %s not found",accountNumber));
   }
 
   public CheckingAccountNotFoundException(String message, Throwable cause) {
--- savingservice/src/main/java/de/kadmos/usecase/savingservice/exception/UserNotFoundException.java
+++ savingservice/src/main/java/de/kadmos/usecase/savingservice/exception/UserNotFoundException.java
@@ -1,11 +1,9 @@
 package de.kadmos.usecase.savingservice.exception;
 
-import java.util.Locale;
-
 public class UserNotFoundException extends Exception {
 
   public UserNotFoundException(Integer userId) {
-    super(String.format("User with ID {} not found",userId));
+    super(String.format("User with ID %s not found",userId));
   }
 
   public UserNotFoundException(String message, Throwable cause) {
