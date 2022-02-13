759ce9b52782bcfd38986bdf41cfa11f640bbffd
motnip <>
2022-02-13 16:06:37 +0100

    Update API path

--- savingservice/src/main/java/de/kadmos/usecase/savingservice/controller/CheckingAccountController.java
+++ savingservice/src/main/java/de/kadmos/usecase/savingservice/controller/CheckingAccountController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/a/savings/users/{userId}/accounts/{accountId}")
+@RequestMapping(path = "/savings/users/{userId}/accounts/{accountId}")
 public class CheckingAccountController {
 
   private CheckingAccountServiceInterface accountService;
